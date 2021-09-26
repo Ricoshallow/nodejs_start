9b8204ad2881a31f925837ff82c163dc73c55fa3
Ricoshallow <2297946635@qq.com>
2021-09-26 11:06:56 +0800

    pagination

--- blog/route/admin.js
+++ blog/route/admin.js
@@ -1,5 +1,7 @@
 const express = require('express')
-const {User} = require('../model/user')
+const {
+    User
+} = require('../model/user')
 
 const admin = express.Router()
 admin.get('/', (req, res) => {
@@ -8,19 +10,29 @@ admin.get('/', (req, res) => {
 admin.get('/login', (req, res) => {
     res.render('admin/login')
 })
-admin.get('/user', async(req, res) => {
+admin.get('/user', async (req, res) => {
+    // get current page 
+    let {
+        page
+    } = req.query
+    page = page? parseInt(page) : 1
+    // get total totalData
+    let totalData = await User.countDocuments()
+    // how many data in each page
+    let eachPageData = 5
+    // total page num
+    let totalPage = Math.ceil(totalData / eachPageData)
     // get userlist form database
-    let userList = await User.find()
-    // console.log(userList);
-    res.render('admin/user',{
-        userList
+    let userList = await User.find().skip((page-1)*eachPageData).limit(eachPageData)
+    res.render('admin/user', {
+        userList,totalPage,page,totalData
     })
 })
-admin.get('/user-edit',(req,res)=>{
+admin.get('/user-edit', (req, res) => {
     res.render('admin/user-edit')
 })
-admin.get('/logout',(req,res)=>{
-    req.session.destroy(()=>{
+admin.get('/logout', (req, res) => {
+    req.session.destroy(() => {
         res.clearCookie('connect.sid')
         res.redirect('/admin/login')
     })
